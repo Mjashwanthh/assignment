e3b772dbc558f1305a771d875cffcd952fea501a
Jashwanth <jashwanthmallela537@gmail.com>
2023-02-14 19:47:17 +0530

    creating resume

---
+++ resume.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5010;
+
+app.get("/", function(req, res){
+
+	res.sendFile(__dirname+"/index.html");
+
+});
+
+app.listen(port, function(){
+	console.log("Server running on http://localhost:"+port);
+	console.log(`Server running on http://localhost:${port}`);
+});
